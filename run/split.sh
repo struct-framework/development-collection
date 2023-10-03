8b0ef42abfd9e10563b6b5aa6bee6a4b95066885
Timo Poppinga <timo.poppinga@zdrei.com>
2023-10-03 12:29:39 +0200

    add readme

--- run/split.sh
+++ run/split.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
-git subtree split -P packages/struct              -b struct
-git subtree split -P packages/struct-contracts    -b struct-contracts
-git subtree split -P packages/data-type           -b data-type
-git subtree split -P packages/data-type-contracts -b data-type-contracts
+git subtree split -P packages/contracts   -b contracts
+git subtree split -P packages/data-type   -b data-type
+git subtree split -P packages/serializer  -b serializer
+git subtree split -P packages/struct      -b struct
 
 
-git push struct                 struct:main -f
-git push struct-contracts       struct-contracts:main -f
-git push data-type              data-type:main -f
-git push data-type-contracts    data-type-contracts:main -f
 
-git branch -D struct
+git push contracts   contracts:main -f
+git push data-type   data-type:main -f
+git push serializer  serializer:main -f
+git push struct      struct:main -f
+
+
+git branch -D contracts
 git branch -D data-type
-git branch -D data-type-contracts
+git branch -D serializer
+git branch -D struct
+
+
