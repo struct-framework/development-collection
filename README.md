1fff6c2cf0607ee212353e25a34cc5f968d8f273
Timo Poppinga <timo.poppinga@zdrei.com>
2023-10-03 15:51:29 +0200

    add deplyment for opperator

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
     git remote add contracts git@github.com:timo-poppinga/struct-contracts.git
     git remote add data-type git@github.com:timo-poppinga/struct-data-type.git
+    git remote add operator git@github.com:timo-poppinga/struct-operator.git
     git remote add serializer git@github.com:timo-poppinga/struct-serializer.git
     git remote add struct git@github.com:timo-poppinga/struct.git
--- run/split.sh
+++ run/split.sh
@@ -2,6 +2,7 @@
 
 git subtree split -P packages/contracts   -b contracts
 git subtree split -P packages/data-type   -b data-type
+git subtree split -P packages/operator    -b operator
 git subtree split -P packages/serializer  -b serializer
 git subtree split -P packages/struct      -b struct
 
@@ -9,12 +10,14 @@ git subtree split -P packages/struct      -b struct
 
 git push contracts   contracts:main -f
 git push data-type   data-type:main -f
+git push operator    operator:main -f
 git push serializer  serializer:main -f
 git push struct      struct:main -f
 
 
 git branch -D contracts
 git branch -D data-type
+git branch -D operator
 git branch -D serializer
 git branch -D struct
 
