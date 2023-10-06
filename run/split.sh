5915899b1e92e8d49acddef85e0f5740ff9a1528
Timo Poppinga <timo.poppinga@zdrei.com>
2023-10-06 09:35:18 +0200

    fix split

--- run/split.sh
+++ run/split.sh
@@ -1,24 +1,36 @@
 #!/usr/bin/env bash
 
-git subtree split -P packages/contracts   -b contracts
-git subtree split -P packages/data-type   -b data-type
-git subtree split -P packages/operator    -b operator
-git subtree split -P packages/serializer  -b serializer
-git subtree split -P packages/struct      -b struct
+git subtree split -P packages/contracts/data-type   -b contracts-data-type
+git subtree split -P packages/contracts/operator    -b contracts-operator
+git subtree split -P packages/contracts/serialize   -b contracts-serialize
+git subtree split -P packages/contracts/struct      -b contracts-struct
+
+git subtree split -P packages/implementation/data-type  -b data-type
+git subtree split -P packages/implementation/operator   -b operator
+git subtree split -P packages/implementation/serialize  -b serialize
+git subtree split -P packages/implementation/struct     -b struct
 
 
 
-git push contracts   contracts:main -f
-git push data-type   data-type:main -f
-git push operator    operator:main -f
-git push serializer  serializer:main -f
-git push struct      struct:main -f
+git push contracts-data-type  contracts-data-type:main -f
+git push contracts-operator   contracts-operator:main -f
+git push contracts-serialize  contracts-serialize:main -f
+git push contracts-struct     contracts-struct:main -f
+
+git push data-type            data-type:main -f
+git push operator             operator:main -f
+git push serialize            serialize:main -f
+git push struct               struct:main -f
 
 
-git branch -D contracts
+git branch -D contracts-data-type
+git branch -D contracts-operator
+git branch -D contracts-serialize
+git branch -D contracts-struct
+
 git branch -D data-type
 git branch -D operator
-git branch -D serializer
+git branch -D serialize
 git branch -D struct
 
 
